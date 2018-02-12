c1bea78844b67d70cb92b4af89670da6c99757f2
QUIROZ NIEVA andres <quirozni@informatique.univ-paris-diderot.fr>
2018-02-12 22:26:07 +0100

    Class Read_class to get the edges of the a graph(stanford format). sors_graph is a script who sort the file(graph) by nodes.

---
+++ src/Read_Graph.cpp
@@ -0,0 +1,81 @@
+//
+//  Read_Graph.cpp
+//  Project_Main
+//
+//  Created by andres quiroz on 12/02/2018.
+//  Copyright © 2018 Andres Quiroz . All rights reserved.
+//
+
+#include "Read_Graph.hpp"
+
+Read_graph::Read_graph(const char * path,bool is_short):edge(1,10){
+    int pid;
+    if(!is_short){
+        int pipefd[2];
+        long len;
+        char * buf=new char[PATH_MAX];
+        if (pipe(pipefd) == -1) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+        switch ( pid = fork() ) {
+            case -1:
+                perror("fork");
+                exit(EXIT_FAILURE);
+            case 0:
+                close(pipefd[0]);
+                
+                if(dup2(pipefd[1],1)<0){
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+                
+                if(execlp("/Volumes/DD2/Master2/MAIN/Project_Main/Project_Main/sort_graph.sh", "/Volumes/DD2/Master2/MAIN/Project_Main/Project_Main/sort_graph.sh", path)<0){
+                    perror("execlp");
+                    exit(EXIT_FAILURE);
+                }
+                
+            default:
+                close(pipefd[1]);
+                wait(NULL);
+                len=read(pipefd[0], buf, PATH_MAX);
+                if(len<0){
+                    perror("read");
+                    exit(EXIT_FAILURE);
+                }
+                close(pipefd[0]);
+        }
+        buf[len-1]='\0';
+        
+        graph.open((const char *)buf);
+        delete[] buf;
+    }
+    
+    else {
+        graph.open((const char *)path);
+    }
+};
+
+pair<int,int> * Read_graph::get_edge(){
+    string line;
+    stringstream ss;
+    while(!graph.eof()){
+        getline(graph,line);
+        for(string::iterator it=line.begin(); it!=line.end(); ++it){
+            if(*it=='\n' || *it=='#' || *it==' '){
+                break;
+            }
+            ss << line;
+            ss >> edge.first >> edge.second;
+            return &edge;
+        }
+    }
+    cout << endl;
+    return nullptr;
+}
+
+
+Read_graph::~Read_graph(){
+    cout << "delete\n";
+    graph.close();
+}
---
+++ src/Read_Graph.hpp
@@ -0,0 +1,33 @@
+//
+//  Read_Graph.hpp
+//  Project_Main
+//
+//  Created by andres quiroz on 12/02/2018.
+//  Copyright © 2018 andres quiroz. All rights reserved.
+//
+
+#ifndef Read_Graph_hpp
+#define Read_Graph_hpp
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sstream>
+
+using namespace std;
+
+class Read_graph{
+public:
+    pair<int,int> * get_edge();
+    Read_graph(const char * path,bool is_short);
+    virtual ~Read_graph();
+private:
+    pair<int, int> edge;
+    ifstream graph;
+};
+
+#endif /* Read_Graph_hpp */
