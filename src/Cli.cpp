6dd97719ef7df0c210c2d8d95a646c7b65535d74
Maestus <bensassi.rached@hotmail.com>
2018-02-13 17:54:03 +0100

    forbidden operation removed

--- src/Cli.cpp
+++ src/Cli.cpp
@@ -1,23 +1,23 @@
 #include "Cli.hpp"
 
-vector<float> Cli::produit_direct(Cli * sparse_matrix, vector<int> x) {
+vector<float> Cli::produit_direct(Cli & sparse_matrix, vector<int> x) {
   vector<float> y;
-  for(int i = 0; i < sparse_matrix->get_nb_node(); i++) {
+  for(int i = 0; i < sparse_matrix.get_nb_node(); i++) {
     y[i] = 0;
-    for(int k = sparse_matrix->get_l()[i]; k < sparse_matrix->get_l()[i+1]; k++) {
-      y[i] += sparse_matrix->get_c()[k] * x[sparse_matrix->get_i()[k]];
+    for(int k = sparse_matrix.get_l()[i]; k < sparse_matrix.get_l()[i+1]; k++) {
+      y[i] += sparse_matrix.get_c()[k]->get_valLine() * x[sparse_matrix.get_i()[k]];
     }
   }
 
   return y;
 }
 
-vector<float> Cli::produit_transpose(Cli * sparse_matrix, vector<int> x) {
+vector<float> Cli::produit_transpose(Cli & sparse_matrix, vector<int> x) {
   vector<float> y;
-  for(int i = 0; i < sparse_matrix->get_nb_node(); i++) y[i] = 0;
-  for(int i = 0; i < sparse_matrix->get_nb_node(); i++) {
-    for(int j = sparse_matrix->get_l()[i]; j < sparse_matrix->get_l()[i+1]; j++) {
-      y[sparse_matrix->get_i()[j]] += sparse_matrix->get_c()[j] * x[i];
+  for(int i = 0; i < sparse_matrix.get_nb_node(); i++) y[i] = 0;
+  for(int i = 0; i < sparse_matrix.get_nb_node(); i++) {
+    for(int j = sparse_matrix.get_l()[i]; j < sparse_matrix.get_l()[i+1]; j++) {
+      y[sparse_matrix.get_i()[j]] += sparse_matrix.get_c()[j]->get_valLine() * x[i];
     }
   }
 
--- src/Cli.hpp
+++ src/Cli.hpp
@@ -1,20 +1,19 @@
-#include <vector>
 #include <array>
+#include <vector>
 #include "Unitval.hpp"
 using namespace std;
 
 class Cli {
 public:
-  Cli(int);
   int get_nb_node() { return nb_node; }
-  vector<Unitval &> get_c() { return c; }
+  vector<Unitval*> get_c() { return c; }
   vector<int> get_l() { return l; }
   vector<int> get_i() { return i; }
-  vector<float> produit_direct(Cli *, vector<int>);
-  vector<float> produit_transpose(Cli *, vector<int>);
+  vector<float> produit_direct(Cli &, vector<int>);
+  vector<float> produit_transpose(Cli &, vector<int>);
 private:
   int nb_node;
-  vector<Unitval &> c;
+  vector<Unitval*> c;
   vector<int> l;
   vector<int> i;
 };
