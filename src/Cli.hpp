5ffbbfca67b4e837a0a64a05b05d54c3f442e281
QUIROZ NIEVA andres <quirozni@informatique.univ-paris-diderot.fr>
2018-02-13 17:56:08 +0100

    Debug project.

--- src/Cli.hpp
+++ src/Cli.hpp
@@ -1,20 +1,25 @@
+#ifndef Cli_hpp
+#define Cli_hpp
+
 #include <vector>
 #include <array>
 #include "Unitval.hpp"
+
 using namespace std;
 
 class Cli {
 public:
   Cli(int);
   int get_nb_node() { return nb_node; }
-  vector<Unitval &> get_c() { return c; }
+  vector<Unitval *> get_c() { return c; }
   vector<int> get_l() { return l; }
   vector<int> get_i() { return i; }
   vector<float> produit_direct(Cli *, vector<int>);
   vector<float> produit_transpose(Cli *, vector<int>);
 private:
   int nb_node;
-  vector<Unitval &> c;
+  vector<Unitval *> c;
   vector<int> l;
   vector<int> i;
 };
+#endif /* Cli_hpp */
