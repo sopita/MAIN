c47d7d4d7da8568f03497097b5194a39bc57ce2d
QUIROZ NIEVA andres <quirozni@informatique.univ-paris-diderot.fr>
2018-02-14 18:37:21 +0100

    Debug script sort_graph.

--- src/sort_graph.sh
+++ src/sort_graph.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-sort -k 1n,1 -k 2n,2 $1 > ${1%/*}/copi_${1##*/}
-echo ${1%/*}/copi_${1##*/}
+if [ ${1%/*} != ${1##*/} ]
+then
+    sort -k 1n,1 -k 2n,2 $1 > ${1%/*}/copi_${1##*/}
+    echo ${1%/*}/copi_${1##*/}
+else
+    sort -k 1n,1 -k 2n,2 $1 > copi_${1##*/}
+    echo copi_${1##*/}
+fi
